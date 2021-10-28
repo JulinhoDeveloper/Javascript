3ee9630227ae4e04e92fe013b9ee349d89f3b9e8
JulioDevFullStack <juliodevfullstack@gmail.com>
2021-10-28 16:54:15 -0300

    incondicionais

---
+++ incondicionais.js
@@ -0,0 +1,16 @@
+var nome = "Julio";
+
+
+if(nome == "Julio") {
+    var idade = 90;
+}
+
+console.log(idade)
+
+if(nome == "Julio") {
+    let idade = 90;
+    console.log(idade)
+}
+
+const nomes = {nomes:'Julio', sobrenome: 'Cesar'};
+console.log(nomes);
